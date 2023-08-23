366336a340cdc5ea1b0055d8612ef0d67e6e3b44
Yandr2022 <sharkievich@gmail.com>
2023-08-23 06:19:34 +0300

    P2 - module8 Exceptions: Unit tests, introduction

--- src/main/java/by/Yandr2022/SBJavaCourse/core/Station.java
+++ src/main/java/by/Yandr2022/SBJavaCourse/core/Station.java
@@ -1,2 +1,45 @@
-package by.Yandr2022.SBJavaCourse.core;public class Station {
+package by.Yandr2022.SBJavaCourse.core;
+
+public class Station implements Comparable<Station>
+{
+    private Line line;
+    private String name;
+
+    public Station(String name, Line line)
+    {
+        this.name = name;
+        this.line = line;
+    }
+
+    public Line getLine()
+    {
+        return line;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Station station)
+    {
+        int lineComparison = line.compareTo(station.getLine());
+        if(lineComparison != 0) {
+            return lineComparison;
+        }
+        return name.compareToIgnoreCase(station.getName());
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        return compareTo((Station) obj) == 0;
+    }
+
+    @Override
+    public String toString()
+    {
+        return name;
+    }
 }
--- src/test/java/by/Yandr2022/SBJavaCourse/RouteCalculatorTest.java
+++ src/test/java/by/Yandr2022/SBJavaCourse/RouteCalculatorTest.java
@@ -1,2 +1,38 @@
-package by.Yandr2022.SBJavaCourse;public class RouteCalculatorTest {
+package by.Yandr2022.SBJavaCourse;
+
+import by.Yandr2022.SBJavaCourse.core.Line;
+import by.Yandr2022.SBJavaCourse.core.Station;
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RouteCalculatorTest extends TestCase {
+
+    List<Station> route;
+
+    @Override
+    protected void setUp() throws Exception {//init
+
+        Line line1 = new Line(1, "First");
+        Line line2 = new Line(2, "Second");
+
+        route = new ArrayList<>() {{
+            add(new Station("AAA", line1));
+            add(new Station("BBB", line1));
+            add(new Station("CCC", line2));
+            add(new Station("DDD", line2));
+        }};
+    }
+
+    public void testCalculateDuration(){
+        double expected = 8.5;
+        double actual = RouteCalculator.calculateDuration(route);
+        assertEquals(expected,actual);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {//remove temp Data
+
+    }
 }
